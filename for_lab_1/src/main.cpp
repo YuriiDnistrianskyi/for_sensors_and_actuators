6eb88d6aee5cebc2a5d7ff7d236f58f5f320367f
YuraDnistriansky <yura.dniester@gmail.com>
2026-02-17 20:29:26 +0200

    fix lab_1

--- for_lab_1/src/main.cpp
+++ for_lab_1/src/main.cpp
@@ -1,32 +1,45 @@
 #include <Arduino.h>
+#define DEBOUNCE 300
 
-uint8_t ledPin = D3;
-uint8_t sensorPin = D4;
+uint8_t ledGreenPin = D5;
+uint8_t ledRedPin = D6;
+uint8_t sensorPin = D2;
 
 bool ledPinHigh = 0;
+uint32_t lastReadSensor = 0;
 
 void setup() 
 {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
+  pinMode(ledRedPin, OUTPUT);
 }
 
 void loop() 
 {
-  uint8_t state = digitalRead(sensorPin);
-  if (state == 1) 
+  uint32_t readSensorTime = millis();
+  if (readSensorTime - lastReadSensor >= DEBOUNCE) 
   {
-    if (ledPinHigh != true) 
+    lastReadSensor = readSensorTime;
+
+    uint8_t state = digitalRead(sensorPin); //
+    if (state == 1) 
     {
-      ledPinHigh = true;
-      digitalWrite(ledPin, HIGH);
+      if (ledPinHigh != true) 
+      {
+        ledPinHigh = true;
+        digitalWrite(ledGreenPin, HIGH);
+        digitalWrite(ledRedPin, LOW);
+      }
     }
-  }
-  else 
-  {
-    if (ledPinHigh != false) 
+    else 
     {
-      ledPinHigh = false;
-      digitalWrite(ledPin; LOW);
+      if (ledPinHigh != false) 
+      {
+        ledPinHigh = false;
+        digitalWrite(ledGreenPin, LOW);
+        digitalWrite(ledRedPin, HIGH);
+      }
     }
+
   }
 }

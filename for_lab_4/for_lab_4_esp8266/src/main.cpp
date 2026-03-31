95dec14a9591ed47cbf1ac08d1739983f831005b
YuraDnistriansky <yura.dniester@gmail.com>
2026-03-31 22:09:58 +0300

    update lab4

--- for_lab_4/for_lab_4_esp8266/src/main.cpp
+++ for_lab_4/for_lab_4_esp8266/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const uint8_t dht11Pin = D6;
+const uint8_t dht22Pin = D7;
+
+DHT dht11(dht11Pin, DHT11);
+DHT dht22(dht22Pin, DHT22);
+
+const uint32_t readSensorsDebounceTime = 2000;
+uint32_t lastReadSensorsTime = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  dht11.begin();
+  dht22.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  uint32_t nowTime = millis();
+  if ((nowTime - lastReadSensorsTime) >= readSensorsDebounceTime)
+  {
+    lastReadSensorsTime = nowTime;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    float t11 = dht11.readTemperature();
+    float t22 = dht22.readTemperature();
+    
+    float h11 = dht11.readHumidity();
+    float h22 = dht22.readHumidity();
+
+    Serial.println("DHT11 Temperature: " + String(t11) + "℃");
+    Serial.println("DHT22 Temperature: " + String(t22) + "℃");
+    
+    Serial.println("DHT11 Humidity: " + String(h11) + "%");
+    Serial.println("DHT22 Humidity: " + String(h22) + "%");
+  }
 }

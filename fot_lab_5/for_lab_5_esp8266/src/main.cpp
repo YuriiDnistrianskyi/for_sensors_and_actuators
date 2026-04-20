7b73441517984a3fce2b94a38f2ea10692bf4315
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-20 13:32:59 +0300

    fix

--- fot_lab_5/for_lab_5_esp8266/src/main.cpp
+++ fot_lab_5/for_lab_5_esp8266/src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-const uint8_t SERVO_PIN = D1;
-const uint8_t POTENTIOMETER_PIN = D2;
-const uint16_t READ_POTENTIOMETER_DEBOUNCE = 200;
+const uint8_t SERVO_PIN = D4;
+const uint8_t POTENTIOMETER_PIN = A0;
+const uint16_t READ_POTENTIOMETER_DEBOUNCE = 50;
 const uint16_t SERIAL_PRINT_DEBOUNCE = 200;
 
 Servo myServo;
@@ -14,19 +14,19 @@ uint32_t lastSerialPrintTime = 0;
 void setup() {
   Serial.begin(115200);
   delay(200);
-  myServo.attach(SERVO_PIN);
-  myServo.write(90);
+  myServo.attach(SERVO_PIN, 500, 2500);
+  myServo.write(0);
 }
 
 void loop() {
   uint32_t currentTime = millis();
   if (currentTime - lastReadPotentiometer > READ_POTENTIOMETER_DEBOUNCE) 
-  {
+  { 
     lastReadPotentiometer = currentTime;
 
     uint16_t potentiometerValue = analogRead(POTENTIOMETER_PIN);
-    uint16_t targetAngle = map(potentiometerValue, 0, 1023, 0, 180); // 1023 -> 255 (254)
-    targetAngle = constrain(targetAngle, 0, 180);
+    uint16_t targetAngle = map(potentiometerValue, 0, 1023, 0, 180);
+    // targetAngle = constrain(targetAngle, 0, 180);
 
     if (targetAngle != lastAngle) 
     {
